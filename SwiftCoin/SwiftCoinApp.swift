8d00c9ec869d1081b2831d4454205760dcfae239
PulkitDhirana1211 <pulkitdhirana9@gmail.com>
2023-07-03 20:54:52 +0530

    Initial Commit

---
+++ SwiftCoin/SwiftCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCoinApp.swift
+//  SwiftCoin
+//
+//  Created by Pulkit Dhirana on 03/07/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
